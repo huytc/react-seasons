f005c8ca92e9b4bdf62f2045e4829c107f489814
Huy Tran <tc.huy.7@gmail.com>
2019-02-19 10:07:46 +0700

    use hook

--- src/index.js
+++ src/index.js
@@ -2,45 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, error] = useLocation();
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude, errorMessage: '' });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return (
-        <div className="ui negative message">
-          <div className="header">Error</div>
-          <p>{this.state.errorMessage}</p>
-        </div>
-      );
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-    
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return (
-      <div className="border red">
-        {this.renderContent()}
+  let content;
+  if (error) {
+    content = (
+      <div className="ui negative message">
+        <div className="header">Error</div>
+        <p>{error}</p>
       </div>
     );
+  } else if (lat) {
+    content = (
+      <SeasonDisplay lat={lat} />
+    );
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
-}
+
+  return <div className="border red">{content}</div>
+};
+
+// class App extends React.Component {
+//   state = { lat: null, errorMessage: '' };
+
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       position => {
+//         this.setState({ lat: position.coords.latitude, errorMessage: '' });
+//       },
+//       err => {
+//         this.setState({ errorMessage: err.message });
+//       }
+//     );
+//   }
+
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return (
+//         <div className="ui negative message">
+//           <div className="header">Error</div>
+//           <p>{this.state.errorMessage}</p>
+//         </div>
+//       );
+//     }
+
+//     if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat} />;
+//     }
+    
+//     return <Spinner message="Please accept location request" />;
+//   }
+
+//   render() {
+//     return (
+//       <div className="border red">
+//         {this.renderContent()}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
